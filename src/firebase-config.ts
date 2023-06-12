aa11bc91e1fa9f4342cc47c464700a663c0bd5a9
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-12 19:19:25 +0100

    commit

---
+++ src/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAF7G2Tf3n52Qz8gBF9w0YdTmB3aBkDy04',
+  authDomain: 'chatapp-typescript.firebaseapp.com',
+  projectId: 'chatapp-typescript',
+  storageBucket: 'chatapp-typescript.appspot.com',
+  messagingSenderId: '1000382193698',
+  appId: '1:1000382193698:web:9bbfb7848297951c8d2a7b',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
