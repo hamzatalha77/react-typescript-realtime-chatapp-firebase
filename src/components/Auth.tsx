5d941b7883679427fd7c339823482a293b5320e1
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-13 15:02:38 +0100

    comm

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+const signInWithGoogle = async () => {}
 const Auth = () => {
-  return <div>Auth</div>
+  return (
+    <div className="auth">
+      <p>Sign In With Google To Continue</p>
+      <button onClick={signInWithGoogle}>Sign in With Google</button>
+    </div>
+  )
 }
 
 export default Auth
