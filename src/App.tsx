ef264788a93608bb3e7b2b160ea1ddc904d09ee4
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-13 18:40:42 +0100

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Auth from './components/Auth'
+import Cookies from 'universal-cookie'
 
+const cookies = new Cookies()
 const App = () => {
+  const [isAuth, setIsAuth] = useState(cookies.get('auth-token'))
   return (
     <>
       <Auth />
