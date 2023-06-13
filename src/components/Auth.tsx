810fbb1f7f24953dd0d7d9b74cf6e5174d24d2fd
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-13 19:01:11 +0100

    commit

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { auth, provider } from '../firebase-config'
 import { signInWithPopup } from 'firebase/auth'
-
+import Cookies from 'universal-cookie'
+const cookies = new Cookies()
 const Auth = () => {
   const signInWithGoogle = async () => {
-    await signInWithPopup(auth, provider)
+    try {
+      const result = await signInWithPopup(auth, provider)
+      cookies.set('auth-token', result.user.refreshToken)
+    } catch (error) {
+      console.log(error)
+    }
   }
   return (
     <div className="auth">
