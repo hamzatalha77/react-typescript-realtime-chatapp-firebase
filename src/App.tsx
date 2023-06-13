eb65a2a127087a65ee3126b7c13fa6271fd52c36
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-13 18:53:51 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Auth from './components/Auth'
 import Cookies from 'universal-cookie'
 
 const cookies = new Cookies()
 const App = () => {
   const [isAuth, setIsAuth] = useState(cookies.get('auth-token'))
-  const [room, setRoom] = useState('')
+  const [room, setRoom] = useState(null)
+  const roomInputRef = useRef(null)
   if (!isAuth) {
     return (
       <>
@@ -20,8 +21,10 @@ const App = () => {
       ) : (
         <div className="room">
           <label>Enter Room Name</label>
-          <input onChange={(e) => setRoom(e.target.value)} />
-          <button>Enter Chat</button>
+          <input ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter Chat
+          </button>
         </div>
       )}
     </>
