8e38c3571375edb03310908241804fd902acefd2
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-13 15:05:26 +0100

    commit

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-const signInWithGoogle = async () => {}
+import { auth, provider } from '../firebase-config'
+import { signInWithPopup } from 'firebase/auth'
+
 const Auth = () => {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, provider)
+  }
   return (
     <div className="auth">
       <p>Sign In With Google To Continue</p>
