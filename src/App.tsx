1ce7311d620c1d1529cd179ccd92855e5a0d08ac
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-14 12:47:46 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -21,14 +21,24 @@ const App = () => {
   return (
     <>
       {room ? (
-        <div className="bg-slate-600 caret-lime-600">Chat</div>
+        <div>Chat</div>
       ) : (
-        <div className="room">
-          <label className="w-8">Enter Room Name</label>
-          <input ref={roomInputRef} />
-          <button onClick={() => setRoom(roomInputRef.current!.value)}>
-            Enter Chat
-          </button>
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <div className="flex flex-col max-w-[600px] w-full">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Enter Room Name
+              </label>
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                ref={roomInputRef}
+              />
+            </div>
+
+            <button onClick={() => setRoom(roomInputRef.current!.value)}>
+              Enter Chat
+            </button>
+          </div>
         </div>
       )}
     </>
