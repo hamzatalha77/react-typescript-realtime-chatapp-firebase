8d81c51a3c6c332c022fdf08bdeddc160f83a1e7
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-06-14 13:45:58 +0100

    commit

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Chat = () => {
-  return <div>Chat</div>
+  const handleSubmit = () => {}
+  return (
+    <div className="chat-app">
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Type Your Message Here..." />
+        <button type="submit">Send!</button>
+      </form>
+    </div>
+  )
 }
 
 export default Chat
